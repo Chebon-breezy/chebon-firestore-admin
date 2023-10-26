0fa1a28acf884a70365f88b39d9756f8820786e2
Brian Chebon <brynnlamura@gmail.com>
2023-10-26 15:31:48 +0300

    Modified app to fetch data

--- src/App.js
+++ src/App.js
@@ -1,34 +1,34 @@
-import React, { useState } from 'react';
-import './firebaseConfig'; // Add this line prevent firebase not loading error
-import { getFirestore, addDoc, collection, getDocs } from "firebase/firestore"; 
+import React, { useState } from "react";
+import "./firebaseConfig"; // Add this line prevent firebase not loading error
+import { getFirestore, addDoc, collection, getDocs } from "firebase/firestore";
 
 function App() {
-  const [inputValue1, setInputValue1] = useState('');
-  const [inputValue2, setInputValue2] = useState('');
+  const [inputValue1, setInputValue1] = useState("");
+  const [inputValue2, setInputValue2] = useState("");
   let [storedValues, setStoredValues] = useState([]);
 
   const db = getFirestore();
 
   const saveDataToFirestore = async () => {
-      const docRef = await addDoc(collection(db, "myCollection"), {
-        field1: inputValue1,
-        field2: inputValue2,
-      });
-      alert("Document written to Database");
+    const docRef = await addDoc(collection(db, "myCollection"), {
+      field1: inputValue1,
+      field2: inputValue2,
+    });
+    alert("Document written to Database");
   };
 
   const fetchDataFromFirestore = async () => {
     const querySnapshot = await getDocs(collection(db, "myCollection"));
     const temporaryArr = [];
     querySnapshot.forEach((doc) => {
-        temporaryArr.push(doc.data());
+      temporaryArr.push(doc.data());
     });
     setStoredValues(temporaryArr);
-};
+  };
 
   return (
     <div className="App">
-      <h1>Save Data to Firebase Firestore</h1>
+      <h1>Admin Save Data</h1>
       <input
         type="text"
         value={inputValue1}
@@ -39,17 +39,19 @@ function App() {
         value={inputValue2}
         onChange={(e) => setInputValue2(e.target.value)}
       />
-      <button onClick={saveDataToFirestore}>Save to Firestore</button> <br/><br/>
-
-      <button onClick={fetchDataFromFirestore}>Fetch from Firestore</button> <br/><br/>
-
+      <button onClick={saveDataToFirestore}>Save to Database</button> <br />
+      <br />
+      <button onClick={fetchDataFromFirestore}>Fetch from Database</button>{" "}
+      <br />
+      <br />
       <div>
-        {storedValues.map( (item, index) => (
+        {storedValues.map((item, index) => (
           <div key={index}>
-            <p>{item.field1}: {item.field2}</p>
+            <p>
+              {item.field1}: {item.field2}
+            </p>
           </div>
-        )
-        )}
+        ))}
       </div>
     </div>
   );
